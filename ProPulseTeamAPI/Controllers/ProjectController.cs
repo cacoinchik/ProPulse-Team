3fbefa347953958b2eef353ad95b28b220349a32
cacoi <cacoi@IBRAGIMOVPC>
2025-02-11 17:04:31 +0300

    Добавьте файлы проекта.

---
+++ ProPulseTeamAPI/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProPulseTeamAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectController : ControllerBase
+    {
+        private static readonly List<string> Projects = new()
+        {
+            "Проект 1",
+            "Проект 2",
+            "Проект 3"
+        };
+
+        [HttpGet]
+        public ActionResult<List<string>> GetProjects()
+        {
+            return Ok(Projects);
+        }
+    }
+}
